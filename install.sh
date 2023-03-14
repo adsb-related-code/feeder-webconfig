2d4eb9adc69851ceee2d610333d36d9c316780a0
ADS-B related code <34561158+ogadsb@users.noreply.github.com>
2023-03-14 09:13:29 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ done
 ipath=/adsbexchange/webconfig
 
 mkdir -p $ipath
-cp adsb-config.txt.webtemplate webconfig.sh leds.sh sanitize-uuid.sh $ipath
+cp adsbfi-config.txt.webtemplate webconfig.sh leds.sh sanitize-uuid.sh $ipath
 cp ./webconfig.service /etc/systemd/system/
 cp ./leds.service /etc/systemd/system/
 rm -f /var/www/html/index.htm*
@@ -46,12 +46,12 @@ cp -r -T ./helpers $ipath/helpers
 chmod a+x $ipath/helpers/*.sh
 cp ./010_www-data /etc/sudoers.d/
 for file in helpers/*.sh; do
-    echo "www-data ALL = NOPASSWD: /adsbexchange/webconfig/$file" >> /etc/sudoers.d/010_www-data
+    echo "www-data ALL = NOPASSWD: /adsbfi/webconfig/$file" >> /etc/sudoers.d/010_www-data
 done
 
 rm -rf /adsbexchange/update
 mkdir -p /adsbexchange
-git clone --depth 1 https://github.com/ADSBexchange/adsbx-update.git /adsbexchange/update
+git clone --depth 1 https://github.com/ogadsb/feeder-update.git /adsbfi/update
 
 if [[ "$1" != "dont_reset_config" ]]; then
     pushd /adsbexchange/update/
